14bb6accbb4bc0d53e9662b1e29f8cb47e7004a9
emmafewer <emmafewer20@gmail.com>
2020-11-19 10:21:20 -0800

    hunter as driver

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
+#require_relative "/lib/bank_account.rb"
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+
+  end  
+
+  def valid?
+    # if sender
+    # end
+  end
+
 end
